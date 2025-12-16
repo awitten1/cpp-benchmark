f0e3fd81ff4d30d7c386d04852efebc96c5e538e
Andrew Witten <awitten007@gmail.com>
2025-12-16 13:15:58 -0500

    now we're memory bound

--- functions.hpp
+++ functions.hpp
@@ -4,31 +4,8 @@
 #include <random>
 #include <vector>
 
-inline std::vector<long> get_nums(size_t num_elems) {
-    std::vector<long> ret(num_elems);
+std::vector<long> get_nums(size_t num_elems);
 
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<long> rng{};
-    std::generate(ret.begin(), ret.end(), [&]() {
-        return rng(gen);
-    });
-    return ret;
-}
+int warmup_cpu();
 
-inline int warmup_cpu() {
-    int j = 0;
-    for (int i = 0; i < 1 << 20; ++i) {
-        j+= rand();
-    }
-    return j;
-}
-
-
-inline long sum(const std::vector<long>& nums) {
-    long ret = 0;
-    for (auto num : nums) {
-        ret += num;
-    }
-    return ret;
-}
+long sum(const std::vector<long>& nums);
--- perf_main.cpp
+++ perf_main.cpp
@@ -1,31 +1,37 @@
 
 #include "functions.hpp"
+#include <chrono>
 #include <csignal>
+#include <ratio>
 #include <string>
 #include <unistd.h>
+#include <iostream>
 
 template<typename Func>
 void RunUnderPerf(Func func) {
-    pid_t cpid = vfork();
     int pid = getpid();
+    pid_t cpid = fork();
     if (cpid == 0) {
         char buf[50];
-        sprintf(buf, "perf stat -p %d > stat.log 2>&1",pid);
+        sprintf(buf, "perf stat -ddd -p %d > stat.log 2>&1",pid);
         execl("/bin/sh", "sh", "-c", buf, NULL);
     }
 
     setpgid(cpid, 0);
     sleep(2);
+    auto now = std::chrono::steady_clock::now();
     func();
-    sleep(2);
+    std::chrono::duration<double, std::milli> diff = std::chrono::steady_clock::now() - now;
     ::kill(-cpid, SIGINT);
+    sleep(2);
+    std::cout << "time " << diff.count() << " ms" << std::endl;
 }
 
 
 int main(int argc, char** argv) {
     auto nums = get_nums(std::stoi(argv[1]));
     RunUnderPerf([nums = std::move(nums)]() {
-        volatile int x = sum(nums);
+        volatile long x = sum(nums);
     });
     return 0;
 }
